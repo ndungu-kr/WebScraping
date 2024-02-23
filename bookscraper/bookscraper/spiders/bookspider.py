3eefbd1819bee740f0b5afffb01de152e2c4beff
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-02-23 13:53:39 +0000

    added comments

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -10,6 +10,7 @@ class BookspiderSpider(scrapy.Spider):
         # Get all the books on page
         books = response.css('article.product_pod')
 
+        # looping through books to get book details and printing to terminal 
         for book in books:
             yield {
                 'name' : book.css('h3 a::text').get(),
@@ -17,11 +18,15 @@ class BookspiderSpider(scrapy.Spider):
                 'url' : book.css('h3 a').attrib['href']
             }
 
+        # generating the next page url
         next_page = response.css('li.next a ::attr(href)').get()
 
+        # creating next page url
         if next_page is not None:
             if 'catalogue/' in next_page:
                 next_page_url = 'https://books.toscrape.com/' + next_page
             else:
                 next_page_url = 'https://books.toscrape.com/catalogue/' + next_page
+
+            # loading the next page and calling parse function 
             yield response.follow(next_page_url, callback= self.parse)
