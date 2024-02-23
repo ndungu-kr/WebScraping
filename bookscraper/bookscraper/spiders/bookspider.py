6e27f1122390d8372e3e9158bd22e7d55a844daa
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-02-23 13:29:32 +0000

    updated parse function to return details of books in first page

--- bookscraper/bookscraper/spiders/bookspider.py
+++ bookscraper/bookscraper/spiders/bookspider.py
@@ -7,4 +7,12 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        # Get all the books on page
+        books = response.css('article.product_pod')
+
+        for book in books:
+            yield {
+                'name' : book.css('h3 a::text').get(),
+                'price' : book.css('.product_price .price_color::text').get(),
+                'url' : book.css('h3 a').attrib['href']
+            }
