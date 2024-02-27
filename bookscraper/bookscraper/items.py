929a44ab0aa70d19a38c158cdf3192ddf1624804
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-02-27 13:45:00 +0000

    uncommenting name variable

--- bookscraper/bookscraper/items.py
+++ bookscraper/bookscraper/items.py
@@ -8,7 +8,7 @@ import scrapy
 
 class BookscraperItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    name = scrapy.Field()
     pass
 
 class BookItem(scrapy.Item):
