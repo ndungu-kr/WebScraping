3e8267e3559c55e1fd85ad913127e3f2d55e7640
ndungu-kr <85244705+ndungu-kr@users.noreply.github.com>
2024-02-29 13:01:29 +0000

    Initialising db and creating table if it doesnt exist through pipeline

--- bookscraper/bookscraper/pipelines.py
+++ bookscraper/bookscraper/pipelines.py
@@ -65,3 +65,42 @@ class BookscraperPipeline:
             adapter['stars'] = 5
 
         return item
+
+
+import mysql.connector
+
+class SaveToMySQLPipeline:
+    
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            password = 'password',
+            database = 'books'
+        )
+
+        # Create cursor, used to execute commands
+        self.cur = self.conn.cursor()
+
+        # Create books table if it doesn't exist
+        self.cur.execute("""
+        CREATE TABLE IF NOT EXISTS books(
+            id int NOT NULL auto_increment, 
+            url VARCHAR(255),
+            title text,
+            upc VARCHAR(255),
+            product_type VARCHAR(255),
+            price_excl_tax DECIMAL,
+            price_incl_tax DECIMAL,
+            tax DECIMAL,
+            price DECIMAL,
+            availability INTEGER,
+            num_reviews INTEGER,
+            stars INTEGER,
+            category VARCHAR(255),
+            description text,
+            PRIMARY KEY (id)
+        )
+        """)
+
+    
